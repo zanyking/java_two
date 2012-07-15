6efbc6a57843ed01473608b1460601376e16357a
Ian Tsai <zanyking@gmail.com>
2012-07-15 14:05:37 +0800

    add new case

---
+++ 2012/javatwo_demo/src/main/java/idv/zanyking/javatwo/UserVM2.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package idv.zanyking.javatwo;
+
+import java.util.List;
+
+import idv.zanyking.javatwo.model.City;
+import idv.zanyking.javatwo.model.DummyVariableResolver;
+import idv.zanyking.javatwo.model.User;
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+
+/**
+ * @author Ian YT Tsai (zanyking)
+ *
+ */
+@VariableResolver(DummyVariableResolver.class)
+public class UserVM2 {
+	@WireVariable
+	private List<City> cities;
+
+	public List<City> getCities() {
+		return cities;
+	}
+	
+	private User user;
+	@Init
+	public void doInit(){
+		user = new User("Ian", "Tsai");
+	}
+	public User getUser() {
+		return user;
+	}
+	
+	@Command("save")
+	public void saveUser(){
+		
+	}
+	
+	
+	
+	
+}
